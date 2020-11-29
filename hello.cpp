799457c262e38ffad81515d07d7f105875eac9f2
lyl-sy <914139305@qq.com>
2020-11-29 17:33:55 +0800

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: hello.cpp
+    > Author: lyl
+    > Mail: lyl5216@163.com 
+    > Created Time: 2020年11月29日 星期日 17时31分44秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"helloworld!"<<endl;
+	return 0;
+}
